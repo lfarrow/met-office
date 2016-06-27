5f33ec2a10f11ea65b1e244d988d50f75467385d
Lewis Farrow <lewis.farrow@myfuturesbright.com>
2016-06-27 13:42:40 +0100

    Added populated location drop-down list

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@
 var baseUrl = 'http://datapoint.metoffice.gov.uk/public/data/',
 	apiKey = '551f7572-56ec-4090-a97e-11ea833f57b4';
 
-/* Functions */
+/* Elements */
+var locationsElement = $("#location");
+
+/* Helper Functions */
 function makeRequest(url, successCallback, errorCallback) {
 	return $.ajax({
 		url: baseUrl + url + '&key=' + apiKey,
@@ -12,3 +15,34 @@ function makeRequest(url, successCallback, errorCallback) {
 		error: errorCallback
 	});
 };
+
+
+/* Functions */
+function getLocationList() {
+	makeRequest('val/wxfcs/all/json/sitelist?', function(location) {
+			/* Pick out data from object */
+			location = location.Locations.Location;
+
+			/* Sort locations alphabetically */
+			location.sort(function(a, b) {
+			    var nameA = a.name.toLowerCase(),
+			    	nameB = b.name.toLowerCase();
+			    return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
+			});
+
+			/* Loop through locations and append to locations dropdown */
+			$.each(location, function() {
+			    locationsElement.append($("<option />").val(this.id).text(this.name));
+			});
+		},
+		function(data) {
+			console.log(data);
+		}
+	);
+}
+
+/* Initialise App */
+function init() {
+	getLocationList();
+}
+init();
